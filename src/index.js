16bcb5ccd68f1b13200f335a4b076ce364ba9b2c
arjun-bhatt <46908672+arjun-bhatt@users.noreply.github.com>
2025-08-20 10:39:07 -0700

    eslint working with .jsx files now.

--- src/index.js
+++
@@ -1,9 +0,0 @@
-console.log('starting up!');
-import $ from 'jquery';
-import './style.scss';
-
-let time = 0;
-
-setInterval(() => {
-    time += 1;
-    $('#main').html(`You've been on this page for ${  time  } seconds`);}, 1000);
