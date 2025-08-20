181feb17f867ef0bd87f277ce8058e168b6bc5df
arjun-bhatt <46908672+arjun-bhatt@users.noreply.github.com>
2025-08-19 23:53:56 -0700

    starterpack additions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 console.log('starting up!')
 import $ from 'jquery';
 
-$('#main').html('Here we go!');
+let time = 0;
+
+setInterval(() => {
+    time += 1;
+    $('#main').html('You\'ve been on this page for ' + time + ' seconds')}, 1000); 
